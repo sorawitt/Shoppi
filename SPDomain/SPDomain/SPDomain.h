5b2aeb52a3df48f60cd9c2e8ae5b50ce1cc1325d
sorawit <sorawit.tr@gmail.com>
2021-04-28 20:34:37 +0700

    Create SPDomain Module

---
+++ SPDomain/SPDomain/SPDomain.h
@@ -0,0 +1,18 @@
+//
+//  SPDomain.h
+//  SPDomain
+//
+//  Created by Sorawit Trutsat on 28/4/2564 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPDomain.
+FOUNDATION_EXPORT double SPDomainVersionNumber;
+
+//! Project version string for SPDomain.
+FOUNDATION_EXPORT const unsigned char SPDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPDomain/PublicHeader.h>
+
+
