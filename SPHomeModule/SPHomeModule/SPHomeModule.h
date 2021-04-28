eeb7b85d7443d4a8cb43bd5ff5adc58bce6b179c
sorawit <sorawit.tr@gmail.com>
2021-04-28 20:48:36 +0700

    Added Feature Modules

---
+++ SPHomeModule/SPHomeModule/SPHomeModule.h
@@ -0,0 +1,18 @@
+//
+//  SPHomeModule.h
+//  SPHomeModule
+//
+//  Created by Sorawit Trutsat on 28/4/2564 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPHomeModule.
+FOUNDATION_EXPORT double SPHomeModuleVersionNumber;
+
+//! Project version string for SPHomeModule.
+FOUNDATION_EXPORT const unsigned char SPHomeModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPHomeModule/PublicHeader.h>
+
+
