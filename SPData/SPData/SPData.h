3292eda553b8d3dad12da0641c3b9a8163d58b5d
sorawit <sorawit.tr@gmail.com>
2021-04-29 13:57:33 +0700

    Added SPData Module

---
+++ SPData/SPData/SPData.h
@@ -0,0 +1,18 @@
+//
+//  SPData.h
+//  SPData
+//
+//  Created by Sorawit Trutsat on 29/4/2564 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPData.
+FOUNDATION_EXPORT double SPDataVersionNumber;
+
+//! Project version string for SPData.
+FOUNDATION_EXPORT const unsigned char SPDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPData/PublicHeader.h>
+
+
