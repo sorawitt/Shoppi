293a0f85fe36727b1bf664bc6658b22d7fa80878
sorawit <sorawit.tr@gmail.com>
2021-04-29 17:18:36 +0700

    Added Fastlane and Firebase Distribution

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '12.2'
+
+use_frameworks!
+
+workspace 'Shoppi'
+
+def application_pods
+end
+
+target 'ShoppiApp' do
+    project 'ShoppiApp/ShoppiApp.project'
+    application_pods
+end
---
+++ SPInfrastructure/SPInfrastructure/SPInfrastructure.h
@@ -0,0 +1,18 @@
+//
+//  SPInfrastructure.h
+//  SPInfrastructure
+//
+//  Created by Sorawit Trutsat on 29/4/2564 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPInfrastructure.
+FOUNDATION_EXPORT double SPInfrastructureVersionNumber;
+
+//! Project version string for SPInfrastructure.
+FOUNDATION_EXPORT const unsigned char SPInfrastructureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPInfrastructure/PublicHeader.h>
+
+
